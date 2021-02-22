c0da7704f90c0f2bf5a49299d7888c16c137902d
Patrik Keller <git@pkel.dev>
2021-02-22 20:08:10 +0100

    Irmin PoC

--- brainstorm.ml
+++ brainstorm.ml
@@ -1,7 +1,19 @@
+open Lwt.Infix
+
 module Data = struct
-  let posts = ref [ ("id42", "hello world")
-                  ; ("id99", "This is the second test post")
-                  ]
+  module Git_store = Irmin_unix.Git.FS.KV(Irmin.Contents.String)
+
+  let git_config = Irmin_git.config "./_db"
+
+  let posts () =
+    let open Git_store in
+    Repo.v git_config >>=
+    master >>= fun t ->
+    list t [] >>=
+    Lwt_list.map_p (fun (step, tree) ->
+        let%lwt content = Tree.get tree [] in
+        Lwt.return (step, content)
+      )
 end
 
 module View = struct
@@ -10,16 +22,19 @@ module View = struct
   let page content =
     (html (head (title (txt "Brainstorm")) [])
        (body content))
+    |> Lwt.return
 
   let posts () =
+    let%lwt posts = Data.posts () in
     page [ h1 [txt "Posts"]
-         ; ul (List.map (fun (id, _) ->
-               li [a ~a:[a_href ("/post/" ^ id)] [txt id]]) !Data.posts)
+         ; ul ( List.map (fun (id, _) ->
+               li [a ~a:[a_href ("/post/" ^ id)] [txt id]]) posts)
          ]
 
   let post id =
+    let%lwt posts = Data.posts () in
     page [ h1 [txt ("Post ID: " ^ id)]
-         ; p [txt (List.assoc id !Data.posts)]
+         ; p [txt (List.assoc id posts)]
          ; p [a ~a:[a_href "/"] [txt "Home"]]
          ]
 
@@ -66,7 +81,6 @@ module Auth = struct
         | `POST -> (
             (* handle post from login form *)
             try
-              let open Lwt.Infix in
               let%lwt user = Request.urlencoded "user" req >|= Option.get
               and password = Request.urlencoded "password" req >|= Option.get
               in
@@ -83,11 +97,11 @@ module Auth = struct
               |> Lwt.return
             with _ ->
               View.login ~message:"login failed" ()
-              |> Response.of_html |> Lwt.return
+              >|= Response.of_html
           )
         | `GET ->
           (* show login form *)
-          View.login () |> Response.of_html |> Lwt.return
+          View.login () >|= Response.of_html
         | _ ->
           (* redirect get *)
           Response.redirect_to req.target |> Lwt.return
@@ -99,11 +113,11 @@ let () =
   App.empty
   |> App.middleware Auth.middleware
   |> App.get "/" (fun _req ->
-      View.posts () |> Response.of_html |> Lwt.return
+      View.posts () >|= Response.of_html
     )
   |> App.get "/post/:id" (fun req ->
       let id = Router.param req "id" in
-      View.post id |> Response.of_html |> Lwt.return
+      View.post id >|= Response.of_html
     )
   |> App.run_command
   |> ignore
