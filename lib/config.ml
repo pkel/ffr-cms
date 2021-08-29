07a211baa9e2ba57f204956fde27962d636dc395
Patrik Keller <git@pkel.dev>
2021-08-29 20:09:02 +0200

    Automatically create config if missing

--- lib/config.ml
+++ lib/config.ml
@@ -14,8 +14,44 @@ type t =
   ; default_category : string (* default category (folder) *)
   } [@@deriving sexp]
 
+let default_s =
+  {|;; Example configuration
+((repo ./_db) ; unix path to local git repository to-be-managed
+ (static_dir ./static) ; unix path to statically served content
+ (user_file (opium-users)) ; path to user file within repository
+ (content_path (content)) ; path to managed content within git repository
+ ; managed categories ; id is used as folder under content_path
+ (categories (((id einsaetze) (label Einsätze))
+              ((id neues) (label Neuigkeiten))
+             ))
+ (default_category einsaetze) ; default category (id)
+ )
+|}
+
 let t : t =
-  Sexplib.Sexp.load_sexp_conv_exn "./config.sexp" t_of_sexp
+  let path = "./config.sexp" in
+  let rec h first_try =
+    match Sexplib.Sexp.load_sexp_conv_exn path t_of_sexp with
+    | a ->
+      (* TODO: use logs library *)
+      Printf.eprintf "Use configuration: %s\n%!" path;
+      a
+    | exception (Sys_error _ as e) ->
+      if first_try then (
+        (* TODO: use logs library *)
+        Printf.eprintf "Write example configuration: %s\n%!" path;
+        let oc = open_out path in
+        begin
+          try
+            Printf.fprintf oc "%s" default_s;
+            close_out oc
+          with e ->
+            close_out oc;
+            raise e
+        end;
+        h false
+      ) else raise e
+  in h true
 
 let category_assoc t =
   List.map (fun c -> c.id, c.label) t.categories
